f6c652df4efda20a6491e5e574ff17bf470ed1dd
Kiril Spasov <spasov.kiril@gmail.com>
2022-11-18 11:29:16 +0200

    Add project files.

---
+++ DoBetter/FormDoBetter.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace DoBetter
+{
+    partial class FormDoBetter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.Location = new System.Drawing.Point(69, 82);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(175, 57);
+            this.BtnStart.TabIndex = 0;
+            this.BtnStart.Text = "Start";
+            this.BtnStart.UseVisualStyleBackColor = true;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(366, 80);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(371, 309);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormDoBetter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnStart);
+            this.Name = "FormDoBetter";
+            this.Text = "Do Better";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ DoBetter/FormDoBetter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DoBetter
+{
+    public partial class FormDoBetter : Form
+    {
+        public FormDoBetter()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            string path = Application.StartupPath + @"\better.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            string line1 = "";
+            string line = "";
+            int linesCount = 0;
+            int lines = 0;
+
+
+            line1 = streamReader.ReadLine();
+            lines = Convert.ToInt32(line1);
+
+            while (linesCount < lines)
+            {
+                line = streamReader.ReadLine();
+
+                TxtResult.Text += TransformWord(line) + Environment.NewLine;
+
+                linesCount++;
+            }
+        }
+
+        private string TransformWord(string word)
+        {
+            string newWord = "";
+
+            if (word.Substring(word.Length - 2, 2) == "er")
+            {
+                for (int i = 0; i < word.Length - 2; i++)
+                {
+                    newWord += word.Substring(i, 1);
+                }
+                newWord += "est";
+            }
+            else if (word.Substring(word.Length - 1, 1) == "e")
+            {
+                for (int i = 0; i <word.Length; i++)
+                {
+                    newWord += word.Substring(i, 1);
+                }
+                newWord += "r";
+            }
+            else
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    newWord += word.Substring(i, 1);
+                }
+                newWord += "er";
+            }
+
+            return newWord;
+        }
+    }
+}
